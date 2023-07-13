2ee8c6dc5f08f0fd40047a7fd57e38d08505a161
msekinci <mehmetserkanekinci@gmail.com>
2023-07-13 23:57:28 +0300

    feat: project created and elasticclient added

---
+++ src/Elasticsearch.Blog.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Elasticsearch.Blog.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
